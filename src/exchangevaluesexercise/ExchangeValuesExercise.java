0b16fd48a7c83180c688f61a163f5c652a07fd39
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2020-01-17 14:20:39 -0700

    commit

---
+++ src/exchangevaluesexercise/ExchangeValuesExercise.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exchangevaluesexercise;
+
+public class ExchangeValuesExercise
+{
+
+    public static void main(String[] args)
+    {
+        int a;
+        int b;
+        int c;
+        
+        a = 7;
+        b = 5;
+        
+        c = a;
+        a = b;
+        b = c;
+        
+        System.out.println(b);
+    }
+
+}
